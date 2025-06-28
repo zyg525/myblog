8b83530667b6232cb22d467e037c5c0ed7a8b941
zyg <zygijk@163.com>
2025-06-28 18:59:39 +0800

    更新文档

---
+++ 启动博客.bat
@@ -0,0 +1,7 @@
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit cd /d "%~dp0"
+C:
+cd C:\Users\12136\Desktop\myDocs\myblog
+nvm use 20.8.1
+yarn dev:win
+
+pause
